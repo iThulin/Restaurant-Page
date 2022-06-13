3fad7570adaefcefd088698752a4473fd209b5d6
Ian <ian.thulin@icloud.com>
2022-06-13 13:23:54 -0600

    Add CSS minifiaction, remove extra files

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
     entry: {
         index: './src/index.js',
-        print: './src/print.js',
       },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
     plugins: [
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
         title: 'Development',
         }),
@@ -21,14 +17,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    optimization: {
-        runtimeChunk: 'single',
-    },
     module: {
         rules: [
         {
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
         },
         {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
