ffe83fd0321661386e7cf3af11c3d6de1baa9dac
Ian <ian.thulin@icloud.com>
2022-06-13 12:02:14 -0600

    Webpack initialization and configuration

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import './style.css';
+import printMe from './print';
+import Icon from './pizza_oven.png';
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+  
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onClick = printMe;
+
+    //Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+    element.appendChild(btn);
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
