b0e5b7a3d4f0e7ee611c395e76180ce9fe4dc578
Ian <ian.thulin@icloud.com>
2022-06-15 13:28:33 -0600

    Change background image, adjust style colors

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -1,12 +1,27 @@
 import {createHTMLElement, $frame} from '../index.js';
 
 function renderFooter() {
-    const $footer = createHTMLElement('div', 'footer', null, null)
-    $footer.appendChild(createHTMLElement('p', 'attribution', null, 'Created by  '));
+    const $footer = createHTMLElement('div', 'footer')
+
+    const $developerContainer = createHTMLElement('div', 'container')
+
+    $developerContainer.appendChild(createHTMLElement('p', 'attribution', null, 'Developed by'));
     
-    const footerLink = createHTMLElement('a', null, null, 'iThulin');
-    footerLink.href = 'https://github.com/iThulin';
-    $footer.appendChild(footerLink);
+    const developerLink = createHTMLElement('a', null, null, 'iThulin');
+    developerLink.href = 'https://github.com/iThulin';
+    $developerContainer.appendChild(developerLink);
+
+    $footer.appendChild($developerContainer);
+
+    const $photographerContainer = createHTMLElement('div', 'container')
+    $footer.appendChild($photographerContainer);
+
+    $photographerContainer.appendChild(createHTMLElement('p', 'attribution', null, 'Photo by'));
+    
+    const photoLink = createHTMLElement('a', null, null, 'Daniel Lee');
+    photoLink.href = 'https://unsplash.com/@crtvdan';
+    
+    $photographerContainer.appendChild(photoLink);
 
     $frame.appendChild($footer);
 }
