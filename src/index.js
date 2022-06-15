96f38670c4671b055831355abe10e8fd03d38518
Ian <ian.thulin@icloud.com>
2022-06-15 11:13:59 -0600

    Create nav bar, style, and add functionality

--- src/index.js
+++ src/index.js
@@ -1,25 +1,67 @@
-import _ from 'lodash';
+import { renderNavBar } from './navbar.js';
 import './style.css';
 
-function createFramework() {
-  // Create framework divs
-  const content = document.createElement('div');
-  const header = document.createElement('div');
-  const main = document.createElement('div');
-  const footer = document.createElement('div');
-  
-  // Assign class to framework elements
-  content.classList.add('content');
-  header.classList.add('header');
-  main.classList.add('main');
-  footer.classList.add('footer');
+// Creates an element to hold all the content of the webpage.
+// This element is exported to other modules for augmentation.
 
-  // Add framework elements to content container
-  content.appendChild(header);
-  content.appendChild(main);
-  content.appendChild(footer);
-  
-  return content;
+const framework = document.createElement('div');
+document.body.appendChild(framework);
+framework.id = 'frame';
+const $frame = document.getElementById('frame')
+
+function createHTMLElement(type, id, classes, content) {
+  // When passed the parameters this function will return a new HTML element
+  // with all parameters applied.
+  const element = document.createElement(type);
+
+  if (id) element.id = id;
+  if (classes) classes.forEach((newClass) => element.classList.add(newClass));
+  if (content) element.innerText = content;
+
+  return element;
 }
-  
-document.body.appendChild(createFramework());
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function home() {
+  removeAllChildNodes($frame);
+  console.log('Render Home');
+  renderNavBar();
+  //render main
+  //render footer
+}
+
+function menu() {
+  removeAllChildNodes($frame);
+  console.log('Render Menu');
+  renderNavBar();
+  //render menu
+  //render footer
+}
+
+function about() {
+  removeAllChildNodes($frame);
+  console.log('Render About');
+  renderNavBar();
+  //render about
+  //render footer
+}
+
+
+
+home();
+document.addEventListener('click', (element) => {
+  const target = element.target;
+
+  if (target.id == 'HOME') home();
+  if (target.id == 'MENU') menu();
+  if (target.id == 'ABOUT') about();
+});
+
+
+//document.body.appendChild(createFramework());
+export {createHTMLElement, $frame};
---
+++ src/navbar.js
@@ -0,0 +1,18 @@
+import {createHTMLElement, $frame} from './index.js';
+
+function renderNavBar() {
+    const navItems = ['HOME', 'MENU', 'ABOUT'];
+    const $ul = document.createElement('ul');
+
+    navItems.forEach((item) => 
+        $ul.appendChild(createHTMLElement('button', item, ['navButton'], item))
+    );
+
+    const $nav = document.createElement('div');
+    $nav.id = 'nav';
+    $nav.appendChild($ul);
+
+    $frame.appendChild($nav);
+}
+
+export { renderNavBar };
