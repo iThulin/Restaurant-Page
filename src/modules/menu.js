8b22ab54f3bf31dbde1e5d848bce2b93964ba843
Ian <ian.thulin@icloud.com>
2022-06-15 17:06:14 -0600

    Menu Items generate from json data. Formatting needed

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,20 +5,61 @@ function renderMenu() {
     const menuItems = menuData;
     console.log(menuItems)
 
-    const $menuContainer = createHTMLElement('div', 'menuContainer')
-    const $menu = createHTMLElement('div', 'menu')
+    // create menu container to hold all generated menu content
+
+    const $menuContainer = createHTMLElement('div', 'menuContainer');
+    const $menu = createHTMLElement('div', 'menu');
+
+    // create subMenuContainers to hold each type of food
+
+    const $antpastiContainer = createHTMLElement('div', 'subMenu');
+    $menu.appendChild($antpastiContainer);
+    $antpastiContainer.appendChild(createHTMLElement('h2', 'subMenuTitle', null, 'ANTIPASTI'));
+
+    const $pizzaAndCalzonesContainer = createHTMLElement('div', 'subMenu');
+    $menu.appendChild($pizzaAndCalzonesContainer);
+    $pizzaAndCalzonesContainer.appendChild(createHTMLElement('h2', 'subMenuTitle', null, 'PIZZA & CALZONES'))
+
+
 
     // loop through the json to create a header for each type of food
 
+    menuItems.Antipasti.forEach((category) => {
+        //create the container
+
+        const $menuItemContainer = createHTMLElement('div', 'menuItemContainer', ['antipasti']);
+        $antpastiContainer.appendChild($menuItemContainer);
+
+        // create the title, price, and description
+        $menuItemContainer.appendChild(createHTMLElement('h3', 'menuItem', null, category.ID));
+
+        const $subContainer = createHTMLElement('div', 'subContainer');
+        $menuItemContainer.appendChild($subContainer)
+
+        $subContainer.appendChild(createHTMLElement('h4', 'price', null, category.price));
+        $subContainer.appendChild(createHTMLElement('h4', 'menuDescription', null, category.description));
+        
+    });
+
+    menuItems['Pizza & Calzones'].forEach((category) => {
+        //create the container
+
+        const $menuItemContainer = createHTMLElement('div', 'menuItemContainer', ['antipasti']);
+        $antpastiContainer.appendChild($menuItemContainer);
+
+        // create the title, price, and description
+        $menuItemContainer.appendChild(createHTMLElement('h3', 'menuItem', null, category.ID));
+
+        const $subContainer = createHTMLElement('div', 'subContainer');
+        $menuItemContainer.appendChild($subContainer)
+
+        $subContainer.appendChild(createHTMLElement('h4', 'price', null, category.price));
+        $subContainer.appendChild(createHTMLElement('h4', 'menuDescription', null, category.description));
+        
+    });
+
     // loop through each object in header
 
-    // create the title
-
-    // create the price/s
-
-    // create the description
-
-    // appendChild to one layer higher until at the frame
 
 
 
